dc830da6ffacfe2680d8f510b459ed5aa586108b
JuanMugres <jmugres123@gmail.com>
2023-05-01 23:32:35 -0600

    segundo avance raices solo falta gui

--- MATH/raices/main.py
+++ MATH/raices/main.py
@@ -1,16 +1,34 @@
-
-
+import gui
 
 def sec(a,b,c,x0,x1,n):
-    x0 = x0
-    x1 = x1
-    fx0 = a*x0**2+b*x0+c
-    fx1 = a*x1**2+b*x1+c
-    x2 = (fx1(x0 - x1))/(fx0-fx1)
-    fx2 = a*x2**2+b*x2+c
+    x0: float = x0
+    x1: float = x1
+    fx0: float = a*x0**2+b*x0+c
+    fx1: float = a*x1**2+b*x1+c
     for i in range(n):
+        x2: float = x1-(fx1*(x0 - x1))/(fx0-fx1)
+        fx2: float = a*x2**2+b*x2+c
+
+        print(x2)
         x0 = x1
         x1 = x2
+        fx0: float = a*x0**2+b*x0+c
+        fx1: float = a*x1**2+b*x1+c
+    
+    return x2
+
+def newr(a,b,c,x0,x1,n):
+    x0: float = (x0+x1)/2
+
+    fx0: float = a*x0**2+b*x0+c
+    fpx0: float = (a*2)*x0+b
+    for i in range(n):
+        x2: float = x0-(fx0/fpx0)
+
+        print(x2)
+        x0 = x2
+        fx0: float = a*x0**2+b*x0+c
+        fpx0: float = (a*2)*x0+b
     
     return x2
 
@@ -41,6 +59,9 @@ def calcular_error(fx):
 
 
 def main():
-    print(sec(2,3,1,1,3,15))
+    #sec(a,b,c,x0,x1,n)
+    print(sec(1,-3,-4,5,7,3))
+    #newr(a,b,c,x0,x1,n)
+    print(newr(-3,8,-1,2,3,2))
 if __name__ == '__main__':
     main()
